70d097c9d93a45a82e51f1d4a723e1ac277d222b
CN23 <codenation@CN23s-MacBook-Pro.local>
2020-02-07 17:08:27 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// Play drum by keyboard press
+
+// Play music by mouse click
+
+const clap = document.getElementById("clap")
+const hihat = document.getElementById("hihat")
+const kick = document.getElementById("kick")
+const openhat = document.getElementById("openhat")
+const boom = document.getElementById("ride")
+const ride = document.getElementById("ride")
+const snare = document.getElementById("snare")
+const tom = document.getElementById("tom")
+const tink = document.getElementById("tink")
+
+
+
+clap.addEventListener("keypress", () => {
+    
+
+}
